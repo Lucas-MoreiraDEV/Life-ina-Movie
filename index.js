af4f817a8ba9d23cc0f9b4b79bf71b1638291924
Lucas Kauê <lucas.moreira.kdev@gmail.com>
2025-08-30 21:50:27 -0300

    estrutura basica

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTNhY2VlNDI2ZDQ2MjE5YTFlYmNiMmY5MzdkZGMxNiIsIm5iZiI6MTc1NjQ3OTczNC40OTYsInN1YiI6IjY4YjFjMGY2YjljZDIwZjhhZjllMTE2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QPFm_yGe4zzQ_7gK-fRba71M2svRSsnJ9YcuCNI-w-k"
+const apiKey = "c93acee426d46219a1ebcb2f937ddc16"
+const config = {
+    headers: {
+        Authorization: `Bearer ${bearerToken}`,
+    }
+}
+
+
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
